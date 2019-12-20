bc06862b3687b4ee5ed037c86bbd1640a4bcc0c0
tnyprk <tonysj.park97@gmail.com>
2019-12-19 18:27:12 -0800

    Initial Commit

---
+++ tippy2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy2
+//
+//  Created by Tony Park on 12/19/19.
+//  Copyright © 2019 Tony Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tippy2Tests/tippy2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  tippy2Tests.swift
+//  tippy2Tests
+//
+//  Created by Tony Park on 12/19/19.
+//  Copyright © 2019 Tony Park. All rights reserved.
+//
+
+import XCTest
+@testable import tippy2
+
+class tippy2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
